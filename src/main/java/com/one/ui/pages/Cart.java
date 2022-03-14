e8e4d72ed70729087f889eb3729170a05798392a
Vesna Petkovic <vesna.petkovic@gmail.com>
2022-03-14 08:36:10 +0100

    Add skeleton for add to cart test

---
+++ src/main/java/com/one/ui/pages/Cart.java
@@ -0,0 +1,23 @@
+package com.one.ui.pages;
+import com.one.framework.Browser;
+import net.bytebuddy.agent.builder.AgentBuilder;
+
+public class Cart {
+    public Cart(Browser browser) {
+    }
+
+    public void addItem(Browser browser) {
+    }
+
+    public void clickOnCart(Browser browser) {
+    }
+
+    public boolean isProductPresent() {
+    }
+
+    public AgentBuilder.RawMatcher getProductLabel() {
+    }
+
+    public void removeProduct(Browser browser) {
+    }
+}
